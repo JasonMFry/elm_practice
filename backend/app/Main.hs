03c480d7981f9d7c2a05910eeb001557ed6d7ded
Jason Fry <jason@fry.team>
2020-10-12 09:47:11 -0400

    Add example of parsing with cassava

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import qualified Data.ByteString.Lazy as BS
+import Data.Csv
+import qualified Data.Vector as V
 
 main :: IO ()
-main = someFunc
+main = parseCsv
+
+parseCsv :: IO ()
+parseCsv = do
+  csvData <- BS.readFile "salaries.csv"
+  case decodeByName csvData of
+    Left err -> putStrLn err
+    Right (_, v) -> V.forM_ v $ \p ->
+      putStrLn $ name p ++ " earns " ++ show (salary p) ++ " dollars."
+
+data Person = Person
+  { name :: !String
+  , salary :: !Int
+  }
+
+instance FromNamedRecord Person where
+  parseNamedRecord r = Person <$> r .: "name" <*> r .: "salary"
