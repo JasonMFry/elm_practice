915f8491cb27f332d760db6de0359ff6a4e369df
Jason Fry <jason@fry.team>
2020-10-12 11:09:27 -0400

    Update model; Add basic html structure

--- frontend/src/Model.elm
+++ frontend/src/Model.elm
@@ -1,12 +1,45 @@
-module Model exposing (Model, init)
+module Model exposing (Model, Test, init, statusToString)
 
 
 type alias Model =
-    { str : String
-    , int : Int
+    List Test
+
+
+type alias Test =
+    { description : String
+    , status : TestStatus
     }
+
+
+type TestStatus
+    = NotStartedYet
+    | Running
+    | Passed
+    | Failed
 
 
 init : Model
 init =
-    { str = "init", int = 1 }
+    [ { description = "commas are rotated properly", status = NotStartedYet }
+    , { description = "exclamation points stand up straight", status = NotStartedYet }
+    , { description = "run-on sentences don't run forever", status = NotStartedYet }
+    , { description = "question marks curl down", status = NotStartedYet }
+    , { description = "semicolons are adequately waterproof", status = NotStartedYet }
+    , { description = "capital letters can do yoga", status = NotStartedYet }
+    ]
+
+
+statusToString : TestStatus -> String
+statusToString st =
+    case st of
+        NotStartedYet ->
+            "Not Started Yet"
+
+        Running ->
+            "Running"
+
+        Passed ->
+            "Passed"
+
+        Failed ->
+            "Failed"
--- frontend/src/View.elm
+++ frontend/src/View.elm
@@ -1,9 +1,29 @@
 module View exposing (view)
 
-import Html exposing (Html, main_, text)
-import Model exposing (Model)
+import Html exposing (Html, button, h1, h2, li, main_, ol, p, span, text)
+import Model exposing (..)
 
 
 view : Model -> Html msg
 view model =
-    main_ [] [ text model.str, text " ", text <| String.fromInt model.int ]
+    main_ []
+        [ h1 [] [ text "Tests" ]
+        , button [] [ text "Start Tests" ]
+        , span []
+            [ p [] [ text "passed: " ]
+            , p [] [ text "failed: " ]
+            , p [] [ text "running: " ]
+            ]
+        , h2 [] [ text "FINISHED!" ]
+        , ol [] (List.map renderTest model)
+        ]
+
+
+renderTest : Test -> Html msg
+renderTest test =
+    li [] [ span [] [ text (statusToString test.status ++ ":   "), text test.description ] ]
+
+
+
+-- sortTests test1 test2 =
+--     todo
